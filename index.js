96f1eda327871404db9e5f5151ae39bb128a2696
Amanda  Crispim <90980799+AmandaCrispim@users.noreply.github.com>
2024-01-16 12:30:27 +0000

    configuração inicial do projeto

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const canvas = document.querySelector('canvas');
-const c = canvas.getContext('2d');
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
 canvas.width = 1824
 canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
